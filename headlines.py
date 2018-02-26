8e6cd5869d2feeddcc670e4ff7b918d3c5d37354
logan <gufengxiaoyuehan@163.com>
2018-02-26 21:35:43 +0800

    modified

--- headlines.py
+++ headlines.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from flask import Flask
 from flask import render_template
+from flask import request
 import feedparser
 
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
@@ -12,8 +13,12 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/<publication>')
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed["entries"][0]
     return render_template("home.html", articles=feed["entries"]
@@ -21,4 +26,4 @@ def get_news(publication='bbc'):
 
 
 if __name__ == "__main__":
-    app.run(port=5000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
